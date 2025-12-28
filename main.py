bf05591622501a2ed8413d126caa75931b247c51
Joseph <Jkhassan95@gmail.com>
2025-12-28 12:08:37 +0000

    Add word list and implement word filtering logic
    Added a new file 'words.txt.txt' containing a list of 5-letter words. Updated main.py to include functions for loading 5-letter words from the file and checking if a word matches guess feedback, supporting logic for green, yellow, and gray feedback.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-def load_words(filename):
-    with open(filename, 'r') as f:
-        return [line.strip().lower() for line in f.readlines()]
+def load_words(all_words):
+    with open('words.txt', 'r') as f:
+        all_words = [line.strip().lower() for line in file if len(line.strip()) == 5]
+
+print(f"Loaded {len(all_words)} words!")
+
+def is_possible(word, guess, feedback):
+    for i in range(5):
+        if feedback[i] == "2": #green
+            if word[i] != guess[i]:
+                return False
+        elif feedback[i] == "1": #yelow
+            #must contain the letter, but not in this spot
+            if guess[i] not in word or word[i] == guess[i]:
+                return False
+        elif feedback[i] == '0': #gray
+            #Letter is not in the word
+            if guess[i] in word:
+                return False
+    return True
